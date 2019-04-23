6f6c13589d2869233ff8b88a54b7c3b1dbf564f9
Rose Stough <psithurise@programmer.net>
2019-04-23 22:21:16 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,5 +7,5 @@ def display_board(board = [" "," "," "," "," "," "," "," "," "])
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[6]} "
   puts "-----------"
-  puts " #{board[7]} | #{board[8]} | #{board[9]}  "
+  puts " #{board[7]} | #{board[8]} | #{board[9]} "
 end
